1ced22fe9a9fe1b5ee4d317126316504ec12dc3e
PINIX1101 <fajartrisnopurwanto@yahoo.co.id>
2025-01-15 05:42:45 +0700

    Override Method Duplicate

--- fajaracademy/models/course.py
+++ fajaracademy/models/course.py
@@ -15,3 +15,18 @@ class Course(models.Model):
 
     user_id = fields.Many2one('res.users', string='Responsible User')
     session_ids = fields.One2many('session', 'course_id', string='Session')
+
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', 'Copy of {}%'.format(self.name))]
+        )
+
+        if not copied_count:
+            new_name = 'Copy of {}'.format(self.name)
+        else:
+            new_name = 'Copy of {} ({})'.format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
