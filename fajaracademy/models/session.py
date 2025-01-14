8a45462135eaccb6d93c23e6c85fb4e77e03c79d
PINIX1101 <fajartrisnopurwanto@yahoo.co.id>
2025-01-15 04:59:16 +0700

    Add Field Compute

--- fajaracademy/models/session.py
+++ fajaracademy/models/session.py
@@ -18,6 +18,15 @@ class Session(models.Model):
     partner_id = fields.Many2one('res.partner', string='Instructior', domain="['|',('is_instructor', '=', True), ('partner_category_id', '!=', False)]")
     course_id = fields.Many2one('course', string='Course')
     partner_ids = fields.Many2many('res.partner', string='partner')
+    taken_seats = fields.Float(compute='_compute_taken_seats', string='Taken Seats')
+    
+    @api.depends('number_of_seats','partner_ids')
+    def _compute_taken_seats(self):
+        for rec in self:
+            if rec.number_of_seats:
+                rec.taken_seats = len(rec.partner_ids)/rec.number_of_seats*100
+            else:
+                rec.taken_seats = 0
 
     def action_confirm(self):
         self.state = 'running'
